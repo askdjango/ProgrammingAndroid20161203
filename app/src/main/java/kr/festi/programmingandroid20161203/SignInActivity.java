0ec421e7b0e131878832f6659e1a55fb3e801dc5
Chinseok Lee <allieuslee@gmail.com>
2016-12-03 12:56:29 +0900

    페이스북 로그인 적용

--- app/src/main/java/kr/festi/programmingandroid20161203/SignInActivity.java
+++ app/src/main/java/kr/festi/programmingandroid20161203/SignInActivity.java
@@ -7,6 +7,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
@@ -14,6 +19,7 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -26,6 +32,9 @@ public class SignInActivity extends AppCompatActivity
 
     SignInButton mSigninGoogleButton;
     GoogleApiClient mGoogleApiClient;
+
+    LoginButton mSigninFacebookButton;
+    CallbackManager mFacebookCallbackManager;
 
     static final int RC_GOOGLE_SIGN_IN = 9001;
 
@@ -56,11 +65,35 @@ public class SignInActivity extends AppCompatActivity
                 .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
+
+        mFacebookCallbackManager = CallbackManager.Factory.create();
+
+        mSigninFacebookButton = (LoginButton) findViewById(R.id.sign_in_facebook_button);
+        mSigninFacebookButton.setReadPermissions("email", "public_profile");
+        mSigninFacebookButton.registerCallback(mFacebookCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                String token = loginResult.getAccessToken().getToken();
+                AuthCredential credential = FacebookAuthProvider.getCredential(token);
+                mFirebaseAuth.signInWithCredential(credential);
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebookLogin:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebookLogin:onError", error);
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        mFacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
 
         if ( requestCode == RC_GOOGLE_SIGN_IN ) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
