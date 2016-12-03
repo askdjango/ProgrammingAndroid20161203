70a99d77cad3f9f0ceefff9c9e6b5d4bf81e5189
Chinseok Lee <allieuslee@gmail.com>
2016-12-03 11:11:29 +0900

    SignInActivity에 구글 로그인 버튼을 추가하고, 거추장스러운 코드 제거

---
+++ app/src/main/java/kr/festi/programmingandroid20161203/SignInActivity.java
@@ -0,0 +1,13 @@
+package kr.festi.programmingandroid20161203;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SignInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
