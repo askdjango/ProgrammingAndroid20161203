01fa320083ce583235d4baa66a0c9226a63e8a62
Chinseok Lee <allieuslee@gmail.com>
2016-12-03 11:24:41 +0900

    구글 로그인을 적용했지만, 그 Result 를 받는 콜백은 아직 없었다 ...

--- app/src/main/java/kr/festi/programmingandroid20161203/SignInActivity.java
+++ app/src/main/java/kr/festi/programmingandroid20161203/SignInActivity.java
@@ -1,13 +1,54 @@
 package kr.festi.programmingandroid20161203;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    SignInButton mSigninGoogleButton;
+    GoogleApiClient mGoogleApiClient;
+
+    static final int RC_GOOGLE_SIGN_IN = 9001;
+
+    static final String TAG = SignInActivity.class.getName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        mSigninGoogleButton = (SignInButton) findViewById(R.id.sign_in_google_button);
+        mSigninGoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_GOOGLE_SIGN_IN);
+            }
+        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // 구글 서버에 접속 실패
+        Log.e(TAG, "onConnectionFailed : " + connectionResult);
     }
 }
