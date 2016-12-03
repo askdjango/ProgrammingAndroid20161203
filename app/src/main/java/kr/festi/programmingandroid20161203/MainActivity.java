d9aa4be2347692b71a339a5dc94d543f63553122
Chinseok Lee <allieuslee@gmail.com>
2016-12-03 12:17:09 +0900

    MainActivity에 로그인 유저의 이름과 사진을 표시

--- app/src/main/java/kr/festi/programmingandroid20161203/MainActivity.java
+++ app/src/main/java/kr/festi/programmingandroid20161203/MainActivity.java
@@ -3,8 +3,11 @@ package kr.festi.programmingandroid20161203;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     FirebaseAuth mFirebaseAuth;
     FirebaseUser mFirebaseUser;
 
+    String mUsername;
     String mPhotoUrl;
 
     @Override
@@ -31,12 +35,21 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             // login 상황
-            String name = mFirebaseUser.getDisplayName();
-            Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+            mUsername = mFirebaseUser.getDisplayName();
+            Toast.makeText(this, mUsername, Toast.LENGTH_LONG).show();
 
             if ( mFirebaseUser.getPhotoUrl() != null ) {
                 mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
             }
         }
+
+        TextView usernameTextView = (TextView) findViewById(R.id.name_textview);
+        ImageView photoImageView = (ImageView) findViewById(R.id.photo_imageview) ;
+
+        usernameTextView.setText(mUsername);
+        if ( mPhotoUrl != null ) {
+            Glide.with(this).load(mPhotoUrl).into(photoImageView);
+        }
+
     }
 }
