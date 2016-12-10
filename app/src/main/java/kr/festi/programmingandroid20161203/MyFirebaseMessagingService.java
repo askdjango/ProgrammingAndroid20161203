806fcd232603a5947b3e043312c936f06acdf131
Chinseok Lee <allieuslee@gmail.com>
2016-12-10 10:44:57 +0900

    FCM 의 data 에서 message_body를 찾습니다.

--- app/src/main/java/kr/festi/programmingandroid20161203/MyFirebaseMessagingService.java
+++ app/src/main/java/kr/festi/programmingandroid20161203/MyFirebaseMessagingService.java
@@ -12,17 +12,27 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        if ( remoteMessage.getNotification() != null ) {
-            final String messageBody = remoteMessage.getNotification().getBody();  // 메세지의 내용
-
-            Handler handler = new Handler(Looper.getMainLooper());
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(getApplicationContext(), messageBody, Toast.LENGTH_LONG).show();
-                }
-            });
+        if ( remoteMessage.getData().size() > 0 ) {
+            String messageBody = remoteMessage.getData().get("message_body");
+            if ( messageBody != null ) {
+                toast(messageBody);
+            }
         }
+
+        if ( remoteMessage.getNotification() != null ) {
+            String messageBody = remoteMessage.getNotification().getBody();  // 메세지의 내용
+            toast(messageBody);
+        }
+    }
+
+    void toast(final String message) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
 
